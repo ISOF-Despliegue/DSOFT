6ce981dc953436775b843ba5223ecbf11a892e3b
AbelYong <ZS23014089@estudiantes.uv.mx>
2026-02-20 11:16:20 -0600

    Configuracion de package

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/calculadora', (req, res) => {
   res.send('Servidor funcionando 🚀');
 });
 
